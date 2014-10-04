470e50d4c588abf2e11b721c06ec0db80bebffd3
Claus Skou Nielsen <kalusn@gmail.com>
2014-10-04 13:30:53 +0200

    Fixed js bug

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,8 +1,8 @@
-notepuddingApp.filter('markdown', function($sce) {
+notepuddingApp.filter('markdown', ['$sce', function($sce) {
   return function(input) {
     marked.setOptions({
       breaks: true
     });
     return $sce.trustAsHtml(marked(input));
   };
-});
+}]);
