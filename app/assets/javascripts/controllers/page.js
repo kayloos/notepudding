ae47b65e5d7bd7c1cb21f9a7e53b48a686037a64
Claus Skou Nielsen <kalusn@gmail.com>
2014-11-21 19:39:16 +0100

    hejjj

--- app/assets/javascripts/controllers/page.js
+++ app/assets/javascripts/controllers/page.js
@@ -13,8 +13,28 @@ notepuddingApp.controller('PageCtrl', ['$scope', '$rootScope', '$timeout', 'pad'
 
     user.import(getUser());
 
-    $scope.bob = function() {
-      alert("hej");
+    $scope.select = function(event) {
+      var curve    = event.target,
+          ancestor = $(curve).parent(),
+          bboxRect = createRect(curve.getBBox());
+
+      ancestor.find('.select').remove();
+      ancestor.append(bboxRect);
+      ancestor.html(ancestor.html());
+
+// replace with directive
+
+      return false;
+    };
+
+    var createRect = function(bbox) {
+      var padding = 8,
+          w       = bbox.width + 2*padding,
+          h       = bbox.height + 2*padding,
+          x       = bbox.x - padding,
+          y       = bbox.y - padding;
+
+      return '<rect class="select" stroke="blue" stroke-width="2px" width="' + w + '" height="' + h + '" x="' + x + '" y="' + y + '" />';
     };
   }
 ]);
